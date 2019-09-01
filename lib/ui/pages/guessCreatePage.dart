d0fce53b64bf11a9184795595583c43c4a0dde3b
NoobBaez <haniel_0101@hotmail.com>
2019-09-01 19:22:04 -0400

    Increase the length of the riddle answer

--- lib/ui/pages/guessCreatePage.dart
+++ lib/ui/pages/guessCreatePage.dart
@@ -78,7 +78,7 @@ class GuessCreate extends StatelessWidget {
                       ),
                     ),
                     keyboardType: TextInputType.text,
-                    maxLength: 9,
+                    maxLength: 10,
                     validators: [
                       (val) {
                         RegExp regex = RegExp(r'^[0-9a-zA-Z ]+$');
