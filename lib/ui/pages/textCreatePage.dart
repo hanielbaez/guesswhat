451e182746d08a0a914d7bf29164818db6332e78
NoobBaez <haniel_0101@hotmail.com>
2019-09-09 22:20:19 -0400

    Add description to Text Create Page

--- lib/ui/pages/textCreatePage.dart
+++ lib/ui/pages/textCreatePage.dart
@@ -26,82 +26,108 @@ class TextCreatePage extends StatelessWidget {
           children: <Widget>[
             FormBuilder(
               key: _formKey,
-              child: Column(
-                children: <Widget>[
-                  FormBuilderTextField(
-                    attribute: "riddle",
-                    maxLines: 5,
-                    autofocus: true,
-                    decoration: InputDecoration(
-                      labelText: "Riddle",
-                      hintText:
-                          'Write a riddle that intrigues the entire world',
-                      labelStyle: TextStyle(color: Colors.black),
-                      border: OutlineInputBorder(
-                        borderRadius:
-                            BorderRadius.horizontal(right: Radius.zero),
+              child: Expanded(
+                child: ListView(
+                  children: <Widget>[
+                    FormBuilderTextField(
+                      attribute: "riddle",
+                      maxLines: 5,
+                      autofocus: true,
+                      decoration: InputDecoration(
+                        labelText: "Riddle",
+                        hintText:
+                            'Write a riddle that intrigues the entire world',
+                        labelStyle: TextStyle(color: Colors.black),
+                        border: OutlineInputBorder(
+                          borderRadius:
+                              BorderRadius.horizontal(right: Radius.zero),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius:
+                              BorderRadius.horizontal(right: Radius.zero),
+                        ),
                       ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius:
-                            BorderRadius.horizontal(right: Radius.zero),
+                      autocorrect: false,
+                      validators: [
+                        FormBuilderValidators.minLength(25,
+                            errorText: 'Your message should be longer.'),
+                        FormBuilderValidators.max(200),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    FormBuilderTextField(
+                      attribute: "answer",
+                      decoration: InputDecoration(
+                        labelText: "Answer",
+                        hintText: 'The respective answer to your riddle',
+                        labelStyle: TextStyle(color: Colors.black),
+                        border: OutlineInputBorder(
+                          borderRadius:
+                              BorderRadius.horizontal(right: Radius.zero),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius:
+                              BorderRadius.horizontal(right: Radius.zero),
+                        ),
+                      ),
+                      keyboardType: TextInputType.text,
+                      maxLength: 10,
+                      validators: [
+                        (val) {
+                          RegExp regex = RegExp(r'^[0-9a-zA-Z ]+$');
+                          if (!regex.hasMatch(val) &&
+                              val.toString().isNotEmpty) {
+                            return "No special characters or numbers";
+                          }
+                          return null;
+                        },
+                      ],
+                    ),
+                    FormBuilderTextField(
+                      attribute: "description",
+                      maxLines: 5,
+                      decoration: InputDecoration(
+                        labelText: "Description",
+                        labelStyle: TextStyle(color: Colors.black),
+                        hintText:
+                            'You can write something funny about your riddle',
+                        border: OutlineInputBorder(
+                          borderRadius:
+                              BorderRadius.horizontal(right: Radius.zero),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius:
+                              BorderRadius.horizontal(right: Radius.zero),
+                        ),
+                      ),
+                      maxLength: 350,
+                      maxLengthEnforced: true,
+                      validators: [
+                        FormBuilderValidators.max(350),
+                      ],
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: [Colors.yellow[600], Colors.orange[400]],
+                          begin: const FractionalOffset(0.0, 0.0),
+                          end: const FractionalOffset(1, 0.0),
+                          stops: [0.0, 1.0],
+                          tileMode: TileMode.clamp,
+                        ),
+                      ),
+                      child: FlatButton(
+                        child: Text(
+                          "Submit",
+                          style: TextStyle(color: Colors.black),
+                        ),
+                        onPressed: () async {},
                       ),
                     ),
-                    autocorrect: false,
-                    validators: [
-                      FormBuilderValidators.minLength(25,
-                          errorText: 'Your message should be longer.'),
-                      FormBuilderValidators.max(200),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 10.0,
-                  ),
-                  FormBuilderTextField(
-                    attribute: "answer",
-                    decoration: InputDecoration(
-                      labelText: "Answer",
-                      hintText: 'The respective answer to your riddle',
-                      labelStyle: TextStyle(color: Colors.black),
-                      border: OutlineInputBorder(
-                        borderRadius:
-                            BorderRadius.horizontal(right: Radius.zero),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius:
-                            BorderRadius.horizontal(right: Radius.zero),
-                      ),
-                    ),
-                    keyboardType: TextInputType.text,
-                    maxLength: 10,
-                    validators: [
-                      (val) {
-                        RegExp regex = RegExp(r'^[0-9a-zA-Z ]+$');
-                        if (!regex.hasMatch(val) && val.toString().isNotEmpty) {
-                          return "No special characters or numbers";
-                        }
-                        return null;
-                      },
-                    ],
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                        colors: [Colors.yellow[600], Colors.orange[400]],
-                        begin: const FractionalOffset(0.0, 0.0),
-                        end: const FractionalOffset(1, 0.0),
-                        stops: [0.0, 1.0],
-                        tileMode: TileMode.clamp,
-                      ),
-                    ),
-                    child: FlatButton(
-                      child: Text(
-                        "Submit",
-                        style: TextStyle(color: Colors.black),
-                      ),
-                      onPressed: () async {},
-                    ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ],
