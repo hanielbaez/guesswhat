77cb1e704a11c2ba005f2b164652aac166d6cdcf
NoobBaez <haniel_0101@hotmail.com>
2019-09-26 13:57:26 -0400

    Add easy_localization

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 //Flutter and Dart import
-import 'package:confetti/confetti.dart';
 import 'package:flutter/material.dart';
+import 'package:confetti/confetti.dart';
 import 'dart:math';
 import 'package:provider/provider.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:easy_localization/easy_localization.dart';
 
 //Self import
 import 'package:Tekel/costumTheme.dart';
@@ -13,7 +15,7 @@ import './router.dart' as router;
 
 void main() {
   setPortraitOrientation();
-  return runApp(MyApp());
+  return runApp(EasyLocalization(child: MyApp()));
 }
 
 class MyApp extends StatefulWidget {
@@ -38,32 +40,43 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
+    var data = EasyLocalizationProvider.of(context).data;
     return MultiProvider(
       providers: providers,
-      child: MaterialApp(
-        title: 'Tekel',
-        theme: costumTheme,
-        home: Stack(
-          children: <Widget>[
-            ListenableProvider<ConfettiController>.value(
-              value: controllerTopCenter,
-              child: HomePage(),
-            ),
-            Align(
-              alignment: Alignment.topCenter,
-              child: ConfettiWidget(
-                confettiController: controllerTopCenter,
-                blastDirection: pi / 2,
-                maxBlastForce: 10,
-                minBlastForce: 2,
-                emissionFrequency: 0.05,
-                numberOfParticles: 5,
+      child: EasyLocalizationProvider(
+        data: data,
+        child: MaterialApp(
+          title: 'Tekel',
+          theme: costumTheme,
+          home: Stack(
+            children: <Widget>[
+              ListenableProvider<ConfettiController>.value(
+                value: controllerTopCenter,
+                child: HomePage(),
               ),
-            ),
+              Align(
+                alignment: Alignment.topCenter,
+                child: ConfettiWidget(
+                  confettiController: controllerTopCenter,
+                  blastDirection: pi / 2,
+                  maxBlastForce: 10,
+                  minBlastForce: 2,
+                  emissionFrequency: 0.05,
+                  numberOfParticles: 5,
+                ),
+              ),
+            ],
+          ),
+          onGenerateRoute: router.generateRoute,
+          initialRoute: '/',
+          localizationsDelegates: [
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            EasylocaLizationDelegate(locale: data.locale, path: 'assets/langs')
           ],
+          supportedLocales: [Locale('es', 'US')],
+          locale: data.savedLocale,
         ),
-        onGenerateRoute: router.generateRoute,
-        initialRoute: '/',
       ),
     );
   }
