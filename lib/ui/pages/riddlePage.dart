af1566514f4f69b89d47d7db2c0e9f804106ed80
NoobBaez <haniel_0101@hotmail.com>
2019-09-26 21:18:54 -0400

    Add ListenableProvider<ConfettiController>

--- lib/ui/pages/riddlePage.dart
+++ lib/ui/pages/riddlePage.dart
@@ -1,4 +1,5 @@
 //Flutter and Dart import
+import 'package:confetti/confetti.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_icons/simple_line_icons.dart';
 
@@ -6,6 +7,7 @@ import 'package:flutter_icons/simple_line_icons.dart';
 import 'package:Tekel/ui/widgets/custom/customDrawer.dart';
 import 'package:Tekel/ui/widgets/riddle/riddle.dart';
 import 'package:Tekel/core/model/riddle.dart';
+import 'package:provider/provider.dart';
 
 class RiddlePage extends StatefulWidget {
   final Riddle riddle;
@@ -16,12 +18,23 @@ class RiddlePage extends StatefulWidget {
 }
 
 class _RiddlePageState extends State<RiddlePage> {
-  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+  ConfettiController controllerTopCenter;
+
+  @override
+  void initState() {
+    controllerTopCenter = ConfettiController(duration: Duration(seconds: 5));
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controllerTopCenter.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      key: _scaffoldKey,
       appBar: AppBar(
         leading: IconButton(
           icon: Icon(SimpleLineIcons.getIconData('arrow-left')),
@@ -35,10 +48,13 @@ class _RiddlePageState extends State<RiddlePage> {
         shrinkWrap: true,
         children: <Widget>[
           Container(
-            constraints: BoxConstraints(
-                maxHeight: MediaQuery.of(context).size.height),
-            child: RiddleLayaout(
-              riddle: widget.riddle,
+            constraints:
+                BoxConstraints(maxHeight: MediaQuery.of(context).size.height),
+            child: ListenableProvider<ConfettiController>.value(
+              value: controllerTopCenter,
+              child: RiddleLayaout(
+                riddle: widget.riddle,
+              ),
             ),
           ),
         ],
