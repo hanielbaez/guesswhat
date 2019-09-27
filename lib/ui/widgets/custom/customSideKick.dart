049347694d0bebd668637e7fba96db996d9dcc33
NoobBaez <haniel_0101@hotmail.com>
2019-09-27 12:55:31 -0400

    Add FadeTransition

--- lib/ui/widgets/custom/customSideKick.dart
+++ lib/ui/widgets/custom/customSideKick.dart
@@ -8,11 +8,51 @@ import 'package:Tekel/ui/widgets/riddle/letter.dart';
 import 'package:Tekel/core/viewModel/letterViewModel.dart';
 
 //? I should rename this class
-class CustomSidekick extends StatelessWidget {
+class CustomSidekick extends StatefulWidget {
+  @override
+  _CustomSidekickState createState() => _CustomSidekickState();
+}
+
+class _CustomSidekickState extends State<CustomSidekick>
+    with SingleTickerProviderStateMixin {
+  AnimationController _fadeSourceController;
+  Animation<double> _fadeSourceAnimation;
+
+  @override
+  void initState() {
+    _fadeSourceController =
+        AnimationController(vsync: this, duration: Duration(seconds: 1));
+    _fadeSourceAnimation =
+        Tween(begin: 0.0, end: 1.0).animate(_fadeSourceController);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _fadeSourceController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Consumer<LettersViewModel>(
       builder: (context, model, child) {
+        Widget _sourceWidget = Container();
+
+        if (model.sourceList.isNotEmpty) {
+          _sourceWidget = Wrap(
+            children: model.sourceList
+                .map(
+                  (item) => CustomLetter(
+                    item: item,
+                    model: model,
+                  ),
+                )
+                .toList(),
+          );
+          _fadeSourceController.forward();
+        }
+
         return Column(
           children: [
             TargetLetter(
@@ -23,15 +63,10 @@ class CustomSidekick extends StatelessWidget {
             ),
             model.correctAnswer
                 ? Container()
-                : Wrap(
-                    children: model.sourceList
-                        .map(
-                          (item) => CustomLetter(
-                            item: item,
-                            model: model,
-                          ),
-                        )
-                        .toList()),
+                : FadeTransition(
+                    opacity: _fadeSourceAnimation,
+                    child: _sourceWidget,
+                  )
           ],
         );
       },
--- lib/ui/widgets/custom/customTargetLetter.dart
+++ lib/ui/widgets/custom/customTargetLetter.dart
@@ -16,89 +16,105 @@ class TargetLetter extends StatefulWidget {
 }
 
 class _TargetLetterState extends State<TargetLetter>
-    with SingleTickerProviderStateMixin {
-  AnimationController animationController;
-  Animation<double> animation;
+    with TickerProviderStateMixin {
+  AnimationController _shakeController;
+  AnimationController _fadeTargetController;
+  Animation<double> _shakeAnimation;
+  Animation<double> _fadeTargetAnimation;
 
   @override
   void initState() {
-    animationController =
+    _shakeController =
         AnimationController(vsync: this, duration: Duration(milliseconds: 500));
+    _fadeTargetController =
+        AnimationController(vsync: this, duration: Duration(seconds: 1));
+    _fadeTargetAnimation =
+        Tween(begin: 0.0, end: 1.0).animate(_fadeTargetController);
     super.initState();
   }
 
   @override
   void dispose() {
-    animationController.dispose();
+    _shakeController.dispose();
+    _fadeTargetController.dispose();
     super.dispose();
   }
 
   v.Vector3 _shake() {
-    double progress = animationController.value;
+    double progress = _shakeController.value;
     double offset = sin(progress * pi * 10.0);
     return v.Vector3(offset * 4, 0.0, 0.0);
   }
 
   Widget build(BuildContext context) {
+    if (widget.model.targetHitList.isNotEmpty) {
+      _fadeTargetController.forward();
+    }
+
     if (widget.model.wrongAnswer) {
-      if (!animationController.isAnimating) {
-        animationController.forward();
+      if (!_shakeController.isAnimating) {
+        _shakeController.forward();
       }
     }
-    animation = Tween<double>(
+    _shakeAnimation = Tween<double>(
       begin: 0.0,
       end: 100.0,
-    ).animate(animationController)
+    ).animate(_shakeController)
       ..addStatusListener((status) {
         if (status == AnimationStatus.completed) {
-          animationController.reset();
+          _shakeController.reset();
         }
       });
 
-    return AnimatedBuilder(
-      animation: animation,
-      builder: (context, child) => Transform(
-        transform: Matrix4.translation(_shake()),
-        child: Wrap(
-          children: widget.model.targetHitList.map((item) {
-            bool _isSelected =
-                widget.model.selectedItems.length - 1 >= item.id ? true : false;
+    return FadeTransition(
+      opacity: _fadeTargetAnimation,
+      child: AnimatedBuilder(
+        animation: _shakeAnimation,
+        builder: (context, child) => Transform(
+          transform: Matrix4.translation(_shake()),
+          child: Wrap(
+            children: widget.model.targetHitList.map((item) {
+              bool _isSelected =
+                  widget.model.selectedItems.length - 1 >= item.id
+                      ? true
+                      : false;
 
-            return GestureDetector(
-              onTap: () {
-                if (widget.model.selectedItems.isNotEmpty &&
-                    _isSelected &&
-                    !widget.model.correctAnswer) {
-                  widget.model.deleteLetter(
-                      sourceItemId: widget.model.selectedItems[item.id].id,
-                      targetItemId: item.id);
-                }
-              },
-              child: Container(
-                alignment: Alignment.center,
-                width: 43.5,
-                height: 40,
-                margin: EdgeInsets.all(4),
-                decoration: BoxDecoration(
-                  color: _isSelected
-                      ? Colors.white
-                      : Colors.black.withOpacity(0.15),
-                  border: Border.all(
-                      color: _isSelected
-                          ? widget.model.letterColor()
-                          : Colors.white),
+              return GestureDetector(
+                onTap: () {
+                  if (widget.model.selectedItems.isNotEmpty &&
+                      _isSelected &&
+                      !widget.model.correctAnswer) {
+                    widget.model.deleteLetter(
+                        sourceItemId: widget.model.selectedItems[item.id].id,
+                        targetItemId: item.id);
+                  }
+                },
+                child: Container(
+                  alignment: Alignment.center,
+                  width: 43.5,
+                  height: 40,
+                  margin: EdgeInsets.all(4),
+                  decoration: BoxDecoration(
+                    color: _isSelected
+                        ? Colors.white
+                        : Colors.black.withOpacity(0.15),
+                    border: Border.all(
+                        color: _isSelected
+                            ? widget.model.letterColor()
+                            : Colors.white),
+                  ),
+                  child: Text(
+                    _isSelected
+                        ? '${widget.model.selectedItems[item.id].letter}'
+                        : '',
+                    style: TextStyle(
+                        color: widget.model.letterColor(),
+                        fontWeight: FontWeight.bold),
+                  ),
                 ),
-                child: Text(
-                  _isSelected
-                      ? '${widget.model.selectedItems[item.id].letter}'
-                      : '',
-                  style: TextStyle(
-                      color: widget.model.letterColor(),
-                      fontWeight: FontWeight.bold),
-                ),
-              ),
-            );
-          }).toList(),
+              );
+            }).toList(),
+          ),
         ),
       ),
     );
