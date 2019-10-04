5746e34b389f7c0ad9e49ddffb92236aebeffa23
NoobBaez <haniel_0101@hotmail.com>
2019-10-03 23:43:42 -0400

    Add user side thumbnail generation

--- lib/core/viewModel/riddleCreateModelView.dart
+++ lib/core/viewModel/riddleCreateModelView.dart
@@ -1,12 +1,14 @@
 //Flutter and Dart import
 import 'dart:io';
+import 'package:Tekel/core/services/location.dart';
 import 'package:flutter/material.dart';
 import 'package:mime/mime.dart';
+import 'package:image/image.dart' as img;
+import 'package:path_provider/path_provider.dart';
 import 'package:provider/provider.dart';
 
 //Self import
 import 'package:Tekel/core/services/db.dart';
-import 'package:Tekel/core/services/location.dart';
 
 class RiddleCreateViewModel extends ChangeNotifier {
   Map<String, dynamic> riddle = {
@@ -27,9 +29,26 @@ class RiddleCreateViewModel extends ChangeNotifier {
     loading = true;
     notifyListeners();
 
+    img.Image image = img.decodeImage(File('${file.path}').readAsBytesSync());
+    // Resize the image to a 200x? thumbnail (maintaining the aspect ratio).
+    img.Image thumbnail = img.copyResize(image, width: 200);
+
+    //Get a temporary path
+    Directory tempDir = await getTemporaryDirectory();
+    String tempPath = tempDir.path + '/_thumbnail.png';
+
+    // Save the thumbnail as a PNG
+    var thumbnailFaile = File(tempPath)
+      ..writeAsBytesSync(img.encodePng(thumbnail));
+
     //Upload media to FireStore and return a Dowload Url
     var _imageUrl =
         await Provider.of<DatabaseServices>(context).uploadToFireStore(file);
+
+    var _thumbnailUrl = await Provider.of<DatabaseServices>(context)
+        .uploadToFireStore(thumbnailFaile);
+
+    riddle['thumbnailUrl'] = _thumbnailUrl;
 
     //Get the media Type video/image
     var listSplit = lookupMimeType(file.path).split('/');
