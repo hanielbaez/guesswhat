f593d6176e82daaf9bb30ef0734e63baaa34552d
NoobBaez <haniel_0101@hotmail.com>
2019-11-15 21:00:24 -0400

    Add connection status

--- lib/ui/widgets/riddle/riddle.dart
+++ lib/ui/widgets/riddle/riddle.dart
@@ -1,6 +1,9 @@
 //Flutter import
 import 'dart:async';
 import 'package:confetti/confetti.dart';
+import 'package:connection_status_bar/connection_status_bar.dart';
+import 'package:easy_localization/easy_localization_delegate.dart';
+import 'package:easy_localization/easy_localization_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:screenshot/screenshot.dart';
@@ -38,63 +41,80 @@ class _RiddleLayaoutState extends State<RiddleLayaout> {
 
   @override
   Widget build(BuildContext context) {
-    return Screenshot(
-      controller: screenshotController,
-      child: Card(
-        shape: BeveledRectangleBorder(
-          borderRadius: BorderRadius.zero,
-        ),
-        child: ListView(
-          children: <Widget>[
-            UserBar(
-              user: User.fromMap(widget.riddle.user),
-              timeStamp: widget.riddle.createdAt,
-              address: widget.riddle.address,
-              riddle: widget.riddle,
+    var data = EasyLocalizationProvider.of(context).data;
+    return Stack(
+      children: <Widget>[
+        Screenshot(
+          controller: screenshotController,
+          child: Card(
+            shape: BeveledRectangleBorder(
+              borderRadius: BorderRadius.zero,
             ),
-            ChangeNotifierProvider<VideoViewModel>.value(
-              value: VideoViewModel(riddle: widget.riddle),
-              child: Consumer<VideoViewModel>(
-                builder: (context, model, child) {
-                  return VideoLayaout(
-                    riddle: widget.riddle,
-                    model: model,
-                    shouldTriggerChange: changeNotifier.stream,
-                    confettiController:
-                        Provider.of<ConfettiController>(context),
-                  );
-                },
-              ),
-            ),
-            if (widget.riddle.answer.isNotEmpty)
-              ChangeNotifierProvider<LettersViewModel>.value(
-                value: LettersViewModel(
-                    riddle: widget.riddle,
-                    db: Provider.of(context),
-                    user: Provider.of<AuthenticationServices>(context).profile,
-                    changeNotifier: changeNotifier),
-                child: CustomSidekick(
-                    riddle: widget.riddle, screenShot: screenshotController),
-              ),
-            SizedBox(
-              height: 5.0,
-            ),
-            ActionBar(riddle: widget.riddle),
-            if (widget.riddle.description.isNotEmpty)
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Align(
-                  alignment: Alignment.topLeft,
-                  child:
-                      CustomDescription(text: '${widget.riddle.description}'),
+            child: ListView(
+              children: <Widget>[
+                UserBar(
+                  user: User.fromMap(widget.riddle.user),
+                  timeStamp: widget.riddle.createdAt,
+                  address: widget.riddle.address,
+                  riddle: widget.riddle,
                 ),
-              ),
-            SizedBox(
-              height: 15.0,
+                ChangeNotifierProvider<VideoViewModel>.value(
+                  value: VideoViewModel(riddle: widget.riddle),
+                  child: Consumer<VideoViewModel>(
+                    builder: (context, model, child) {
+                      return VideoLayaout(
+                        riddle: widget.riddle,
+                        model: model,
+                        shouldTriggerChange: changeNotifier.stream,
+                        confettiController:
+                            Provider.of<ConfettiController>(context),
+                      );
+                    },
+                  ),
+                ),
+                if (widget.riddle.answer.isNotEmpty)
+                  ChangeNotifierProvider<LettersViewModel>.value(
+                    value: LettersViewModel(
+                        riddle: widget.riddle,
+                        db: Provider.of(context),
+                        user: Provider.of<AuthenticationServices>(context)
+                            .profile,
+                        changeNotifier: changeNotifier),
+                    child: CustomSidekick(
+                        riddle: widget.riddle,
+                        screenShot: screenshotController),
+                  ),
+                SizedBox(
+                  height: 5.0,
+                ),
+                ActionBar(riddle: widget.riddle),
+                if (widget.riddle.description.isNotEmpty)
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Align(
+                      alignment: Alignment.topLeft,
+                      child: CustomDescription(
+                          text: '${widget.riddle.description}'),
+                    ),
+                  ),
+                SizedBox(
+                  height: 15.0,
+                ),
+              ],
             ),
-          ],
+          ),
         ),
-      ),
+        //Network connection status
+        EasyLocalizationProvider(
+          data: data,
+          child: ConnectionStatusBar(
+            title: Text(
+              AppLocalizations.of(context).tr('connectionStatusBar.title'),
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
